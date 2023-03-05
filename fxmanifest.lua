42573a97ac8cc1cbd45bd8c2de3c2840dce5bccd
Amadeus <102830960+Amadeus5544@users.noreply.github.com>
2023-03-05 11:13:08 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Amadeus'
+description 'Hozzáad egy lőszerdoboz elemet, amely használatakor 20 töltényt ad a fegyveredhez.'
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server.lua'
+}
